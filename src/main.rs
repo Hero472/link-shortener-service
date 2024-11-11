73d8abdb3f14d5f4094e58a252ea1aa2117220b3
David P. <megver83@parabola.nu>
2024-11-11 16:18:51 -0300

    Listen to all interfaces

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> mongodb::error::Result<()> {
             .app_data(web::Data::new(client.clone()))
             .configure(public_routes)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await?;
     
